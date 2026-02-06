a663815dd32e8b4428df25c74fc8cd2dc67d98c8
Karim Mohamed <komicho1996@gmail.com>
2026-02-06 19:08:44 +0200

    Add ObelawPIMServiceProvider and update composer.json for Laravel integration

---
+++ src/Providers/ObelawPIMServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Obelaw\Pim\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ObelawPIMServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        }
+    }
+}
